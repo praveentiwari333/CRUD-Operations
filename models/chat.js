97c7aa3378b512a955788a49705ed284b2a83ae0
praveentiwari333 <bkttiwar@gmail.com>
2024-11-05 21:32:22 +0530

    CRUD Operation

---
+++ models/chat.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const chatSchema =  new mongoose.Schema({
+    from:{
+        type:String,
+        required:true
+    },
+    to:{
+        type:String,
+        required:true
+    },
+    msg:{
+        type:String,
+        maxLength:50
+    },
+    created_at:{
+        type:Date,
+        required:true
+    }
+})
+
+const Chat = mongoose.model("Chat",chatSchema);
+
+module.exports = Chat;
